04d44867e9896842dc09f43949cd43e71b75ebc4
sserges <agounkeserge@gmail.com>
2021-12-21 11:25:18 -0500

    Styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,23 @@ import Form from './components/Form/Form'
 
 import memories from './images/memories.png'
 
+import useStyles from './styles'
+
 const App = () => {
+  const classes = useStyles()
+
   return (
     <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
           Memories
         </Typography>
-        <img src={memories} alt='memories' height='60' />
+        <img
+          className={classes.image}
+          src={memories}
+          alt='memories'
+          height='60'
+        />
       </AppBar>
       <Grow in>
         <Container>
