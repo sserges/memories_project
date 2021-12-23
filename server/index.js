fbf995ac41dbe83dd44014b431dde93910c40b18
sserges <agounkeserge@gmail.com>
2021-12-23 14:13:55 -0500

    Form

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
-
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL =
   'mongodb+srv://emily:P5WE8kMuPoOzXMHU@cluster0.p34fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
