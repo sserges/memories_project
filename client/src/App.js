540828ed48477b27019f1ab82cba9b3df66a7138
sserges <agounkeserge@gmail.com>
2022-01-21 17:04:05 -0500

    Mobile Responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -37,6 +37,7 @@ const App = () => {
         <Container>
           <Grid
             container
+            className={classes.mainContainer}
             justifyContent='space-between'
             alignItems='stretch'
             spacing={3}
